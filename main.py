17199772feefd4a9a29364d163bf6fab3775e09e
Michel Thibault <ton.email@exemple.com>
2025-10-09 08:13:03 -0400

    Ajout du premier script Python

---
+++ main.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+def bonjour():
+    print("👋 Bonjour Michel ! Ceci est ton premier script Python depuis Ubuntu WSL2 et GitHub.")
+
+if __name__ == "__main__":
+    bonjour()
